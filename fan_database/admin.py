e89de814d9e122965c66afb1270e8c555f98472a
IzzyLeech <ralphhunter46@gmail.com>
2023-03-20 14:49:00 +0000

    created model for season and episodes, register the models to the admin and add the str function

--- fan_database/admin.py
+++ fan_database/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Season
+from .models import Episode
 
-# Register your models here.
+admin.site.register(Season)
+admin.site.register(Episode)
--- fan_database/models.py
+++ fan_database/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Season(models.Model):
+    season_number = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    episode_count = models.IntegerField()
+
+    def __str__(self):
+        return self.season_number
+
+
+class Episode(models.Model):
+    overall_episode_number = models.IntegerField()
+    season_episode_number = models.IntegerField()
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
+    title = models.CharField(max_length=220)
+    air_date = models.DateField()
+    stardate = models.IntegerField()
