ef501f25f469f5c2c728504278678a979397414c
IzzyLeech <ralphhunter46@gmail.com>
2023-03-30 15:35:02 +0000

    created view that will allow the user to post in the community.html

--- community/urls.py
+++ community/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('community/', views.community_view, name='community')
+    path('community/', views.community_view, name='community'),
+    path('create-post/', views.create_post, name='create-post'),
 ]
--- community/views.py
+++ community/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import CommunitySection
+from .form import PostForm
 
 # Create your views here.
 
@@ -8,3 +9,17 @@ def community_view(request):
     sections = CommunitySection.objects.all()
     context = {'sections': sections}
     return render(request, 'community.html', context)
+
+
+def create_post(request):
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('community')
+
+    context = {'form': form}
+    return render(request, 'post_form.html', context)
+    
