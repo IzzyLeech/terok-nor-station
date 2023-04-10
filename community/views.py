6ad387c7ad1c5b0f79cfcf5e86d4cca6e4b1145d
IzzyLeech <ralphhunter46@gmail.com>
2023-04-10 10:14:29 +0000

    add to create_post function that will register the request.user as the post.created_by field

--- community/views.py
+++ community/views.py
@@ -24,7 +24,9 @@ def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+            post.created_by = request.user
+            post.save()
             return redirect('community')
 
     context = {'form': form}
