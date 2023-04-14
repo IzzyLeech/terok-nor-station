40c6308b0c336fd7609b71e507cab7e90a0508de
IzzyLeech <ralphhunter46@gmail.com>
2023-04-14 18:04:37 +0000

    changed to Development

--- terok_nor_station/settings.py
+++ terok_nor_station/settings.py
@@ -31,11 +31,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEVELOPMENT")
 
 ALLOWED_HOSTS = ["terok-nor-station.herokuapp.com", "localhost"]
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Application definition
 
