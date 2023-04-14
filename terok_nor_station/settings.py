dd255759be738829c52a481e028f1c2d7f71cc72
IzzyLeech <ralphhunter46@gmail.com>
2023-04-14 17:52:48 +0000

    add DEVELOPMENT to DEBUG

--- terok_nor_station/settings.py
+++ terok_nor_station/settings.py
@@ -31,10 +31,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEVELOPMENT")
 
 ALLOWED_HOSTS = ["terok-nor-station.herokuapp.com", "localhost"]
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Application definition
 
