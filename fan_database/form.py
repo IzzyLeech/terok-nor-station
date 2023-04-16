da6c1c9af326b29c5cd0b3098ccfa1ae5229ff8c
IzzyLeech <ralphhunter46@gmail.com>
2023-04-16 18:14:50 +0000

    fixed the login/logout placement in navbar, fixed height in post description, added default image and if statment to episdoe.html for image if not uploaded

--- fan_database/form.py
+++ fan_database/form.py
@@ -71,7 +71,7 @@ class RegisterForm(UserCreationForm):
 class LoginForm(forms.ModelForm):
     username = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput())
-    
+
     class Meta:
         model = User
         fields = ['username', 'password']
